1f11c0948e5d4492a07985385c729057e1a119b0
Ahmed Emad <ahmedemad755@gmail.com>
2025-11-17 03:38:48 +0200

    finesh cart&payment

---
+++ lib/core/utils/app_key.dart
@@ -0,0 +1,4 @@
+const secretpaypalKey =
+    'EB1zq5TMCMIMDlgOzbE-46RS3MVdvqV7xF_C7niK3VFQp09NTezzisClHSbxo3DI-whovLxKFLQ6eQYX';
+const clientPaypalKeyId =
+    'AZRjNlyfuFOMJ57p7bXCOmM0KZlkUq-8cK1ydPYvlZ_G1ruAdleKT1xFcDsLCSk3lDzhGNu8-8RxsD8W';
